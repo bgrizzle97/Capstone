657dccad69c616ab057d9a20470dde26acea873d
huntercawthorne <63376201+huntercawthorne@users.noreply.github.com>
2021-04-01 18:09:36 -0400

    User account creation and maintaining

---
+++ createUserDb.py
@@ -0,0 +1,5 @@
+import sqlite3
+conn = sqlite3.connect('user.db')
+conn.execute("CREATE TABLE user (id INTEGER PRIMARY KEY, username char(100) NOT NULL, email char(100) NOT NULL, password char(100) NOT NULL)")
+conn.commit()
+conn.close()
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, g
+import sqlite3
+import dataset
+
+import passwords
 
 app = Flask(__name__)
+db = dataset.connect('sqlite:///user.db')
+
+userID = 1
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        if request.form['username'] != 'Tester' or request.form['password'] != 'test':
+        username = request.form['username']
+        password = request.form['password']
+        profile = db['user'].find_one(username=username)
+        if profile == None:
             return redirect(url_for('home'))
-        else:
-            return redirect(url_for('index'))
+            return
+        if (not passwords.verify_password(password, profile["password"])):
+            return redirect(url_for('home'))
+            return
+        
+        return redirect(url_for('index'))
     return render_template('login.html')
 
 @app.route('/index')
@@ -23,12 +37,32 @@ def teamComparison():
 def teamPage():
     return render_template('teamPage.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = passwords.encode_password(request.form['password'])
+        global userID
+        profile = db['user'].find_one(id=userID)
+        while profile != None:
+            userID += 1
+            profile = db['user'].find_one(id=userID)
+        db['user'].insert({'id':userID, 'username':username, 'email':email, 'password':password})
+        return redirect(url_for('home'))
     return render_template('createUser.html')
 
-@app.route('/forgotPassword')
+@app.route('/forgotPassword', methods=['GET', 'POST'])
 def forgotPassword():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = passwords.encode_password(request.form['password'])
+        profile = db['user'].find_one(username=username)
+        if profile == None:
+            return redirect(url_for('forgotPassword'))
+            return
+        db['user'].update({'username':username, 'password':password},['username'])
+        return redirect(url_for('home'))
     return render_template('forgot_password.html')
 
 if __name__ == '__main__':
